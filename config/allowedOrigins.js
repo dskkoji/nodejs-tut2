ef534739a81657a76f1eb461751f4e61984829d7
dskkoji <50835363+dskkoji@users.noreply.github.com>
2024-01-10 18:56:35 +0900

    modified

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,10 @@
 const allowedOrigins = [
-  'https://www.google.com',
-  'http://127.0.0.1.3500',
-  'http://localhost:3500',
-  'http://localhost:5500'
-]
+  "https://www.google.com",
+  "http://127.0.0.1:3500",
+  "http://127.0.0.1:5173",
+  "http://localhost:3500",
+  "http://localhost:5173",
+  "http://localhost:5500",
+];
 
 module.exports = allowedOrigins
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,7 +3,7 @@ const User = require('../model/User')
 
 const getAllUsers = async (req, res) => {
   const users = await User.find()
-  if (!users) return res.status(400).json({ 'message': 'No users found'})
+  if (!users) return res.status(204).json({ 'message': 'No users found'})
   res.json(users)
 }
 
