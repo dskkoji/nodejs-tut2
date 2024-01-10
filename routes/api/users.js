65d1f26ed4a8289324b36614794defac46c66999
dskkoji <50835363+dskkoji@users.noreply.github.com>
2024-01-10 12:46:37 +0900

    commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const usersController = require('../../controllers/usesController')
+const usersController = require('../../controllers/usersController')
 const ROLES_LIST = require('../../config/roles_list')
 const verifyRoles = require('../../middleware/verifyRoles')
 
