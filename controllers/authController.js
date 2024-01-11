e53de1bd93f269776bac88b6d17803e9e5431ddf
dskkoji <50835363+dskkoji@users.noreply.github.com>
2024-01-12 08:29:25 +0900

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,13 +4,13 @@ const jwt = require('jsonwebtoken')
 
 const handleLogin = async (req, res) => {
   const cookies = req.cookies
-
+  console.log(`cookie available at login: ${JSON.stringify(cookies)}`)
   const { user, pwd } = req.body
-
   if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required' })
 
   const foundUser = await User.findOne({ username: user }).exec()
-  if (!foundUser) return res.sendStatus(401)
+  if (!foundUser) return res.sendStatus(401) // Unauthorized
+
   const match = await bcrypt.compare(pwd, foundUser.password)
   if (match) {
     const roles = Object.values(foundUser.roles).filter(Boolean)
@@ -23,24 +23,25 @@ const handleLogin = async (req, res) => {
         }
       },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: '40s' }
+      { expiresIn: '10s' }
     )
+
     const newRefreshToken = jwt.sign(
       { "username": foundUser.username },
       process.env.REFRESH_TOKEN_SECRET,
-      { expiresIn: '15s' }
+      { expiresIn: '1d' }
     )
 
     let newRefreshTokenArray = 
-      !cookies?.jwt 
-        ? foundUser.refreshToken
-        : foundUser.refreshToken.filter(rt => rt !== cookies.jwt)
+       !cookies?.jwt
+          ? foundUser.refreshToken
+          : foundUser.refreshToken.filter(rt => rt !== cookies.jwt)
 
     if (cookies?.jwt) {
       const refreshToken = cookies.jwt
       const foundToken = await User.findOne({ refreshToken }).exec()
-
       if (!foundToken) {
+        console.log('attempted refresh token reuse at login')
         newRefreshTokenArray = []
       }
 
@@ -49,10 +50,11 @@ const handleLogin = async (req, res) => {
 
     foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken]
     const result = await foundUser.save()
+    console.log(result)
 
     res.cookie('jwt', newRefreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 })
 
-    res.json({ accessToken })
+    res.json({ roles, accessToken })
   } else {
     res.sendStatus(401)
   }
